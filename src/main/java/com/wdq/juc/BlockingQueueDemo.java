1109185626a0added8a13a772871785864a24d42
wudq <347562190@qq.comwudq>
2020-09-20 22:38:46 +0800

    动态规划

---
+++ src/main/java/com/wdq/juc/BlockingQueueDemo.java
@@ -0,0 +1,81 @@
+package com.wdq.juc;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 生产者  消费者
+ * 生产一个 消费一个 5次
+ * 虚假唤醒问题
+ */
+public class BlockingQueueDemo {
+    public static void main(String[] args) {
+        Product product = new Product();
+        new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                    product.incr();
+            }
+        },"aa").start();
+        new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                    product.decr();
+            }
+        },"bb").start();
+
+        new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                    product.incr();
+            }
+        },"cc").start();
+        new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                    product.decr();
+            }
+        },"dd").start();
+
+    }
+
+}
+
+
+class Product2 {
+    private int number = 0;
+    private ReentrantLock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+    public void incr() {
+        lock.lock();
+        try {
+            if (number != 0 ) {
+                condition.await();
+            }
+            number++;
+            System.out.println(Thread.currentThread().getName() + "\t"+number);
+            //唤醒所有等待线程，包括wait和blocked两种状态的线程
+            condition.signalAll();
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+
+    }
+
+    public void decr() {
+        lock.lock();
+        try {
+            //虚假唤醒
+            //因为 signalAll()是唤醒所有等待的线程去拿锁，包括wait和blocked两种状态的线程
+            //如果是blocked状态的线程先拿到锁执行number-- 当被await线程拿到锁时number的值已经变了
+            if (number == 0 ) {
+                condition.await();
+            }
+            number--;
+            System.out.println(Thread.currentThread().getName() + "\t"+number);
+            condition.signalAll();
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
---
+++ src/main/java/com/wdq/leetcode/algorithm/动态规划/滑动窗口的最大值.java
@@ -0,0 +1,78 @@
+package com.wdq.leetcode.algorithm.动态规划;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个数组 nums 和滑动窗口的大小 k，请找出所有滑动窗口里的最大值。
+ *
+ * 示例:
+ *
+ * 输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+ * 输出: [3,3,5,5,6,7]
+ * 解释:
+ *
+ *   滑动窗口的位置                最大值
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ *  1 [3  -1  -3] 5  3  6  7       3
+ *  1  3 [-1  -3  5] 3  6  7       5
+ *  1  3  -1 [-3  5  3] 6  7       5
+ *  1  3  -1  -3 [5  3  6] 7       6
+ *  1  3  -1  -3  5 [3  6  7]      7
+ *
+ */
+public class 滑动窗口的最大值 {
+    public static void main(String[] args) {
+        int k = 2;
+        int[] nums = {1,3};
+        int[] maxSlidingWindow = maxSlidingWindow(nums, k);
+        System.out.println(Arrays.toString(maxSlidingWindow));
+    }
+
+    /**
+     * 状态表示
+     * <li>集合定义<li/>
+     * 长度为K且下标连续的子集合
+     * <li>属性<li/>
+     *  最大值
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums.length == 0 || k == 1) {
+            return nums;
+        }
+
+        int maxs[] = new int[nums.length - k + 1];
+        int maxsLen = maxs.length;
+        int temp = 0;
+
+        for (int i = 0; i < k ; i++) {
+            //重新遍历出最大值
+            temp = Math.max(temp, nums[i]);
+        }
+        maxs[0] = Math.max(temp, nums[k - 1]);
+        if (k == nums.length) {
+            return maxs;
+        }
+
+        for (int i = 1; i < maxsLen; i++) {
+            //前一子数组的最大值 小于 当前值
+            if (maxs[i-1] <= nums[i + k -1]) {
+                maxs[i] = nums[i + k -1];
+                //前一子数组的最大值 在当前 子数组中
+            } else if (maxs[i-1] > nums[i-1]) {
+                maxs[i] = Math.max(maxs[i-1], nums[i + k -1]);
+            } else {
+                temp = 0;
+                for (int j = 0; j < k ; j++) {
+                    //重新遍历出最大值
+                    temp = Math.max(temp, nums[i+j]);
+                }
+                maxs[i] = Math.max(temp, nums[i + k -1]);
+            }
+        }
+        return maxs;
+    }
+}
